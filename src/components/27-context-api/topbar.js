8a27fa0003357584209f87f0f2b7a549c02a4397
slymnsarii <slymnsarii@gmail.com>
2023-01-24 22:51:05 +0300

    update

--- src/components/27-context-api/topbar.js
+++ src/components/27-context-api/topbar.js
@@ -4,6 +4,7 @@ import "./topbar.scss";
 
 const Topbar = () => {
   // herhangi bir yerden bu bilgilere ulasmak istiyorsam merkezi state'de oluşturduğum useContext(StoreContex) deyip ulasabiliyorum
+  //useContext hook'u App.js'deki { counter, setCounter, currencies } objesini getiriyor yani StoreContext'i getiriyor
   const store = useContext(StoreContext); //Context'e ulaşmak için bu satır yeterli, counter ve setCounter kullanmayacağım sadece currencies lazım diye kullandık burayı onlar lazım olsa burda onları da yazardım, bunu dediğimiz anda app'deki value'leri alıyor burdaki 3 değeri store'a atıyor
   const { currencies } = store; //aşağıda sürekli store.currencies yazmak yerine(destructing)
 
